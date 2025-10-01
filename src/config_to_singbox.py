0bd0213d7de2602541cdd0717fb220a7294e9dd2
Pun-Pun-03 <109609472+Pun-Pun-03@users.noreply.github.com>
2025-10-01 22:36:05 +0330

    Update config_to_singbox.py

--- src/config_to_singbox.py
+++ src/config_to_singbox.py
@@ -9,7 +9,7 @@ from urllib.parse import urlparse, parse_qs
 
 class ConfigToSingbox:
     def __init__(self):
-        self.output_file = 'configs/singbox_configs.json'
+        self.output_file = 'main'
         self.headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
         }
